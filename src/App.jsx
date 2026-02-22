02d63960a3a914ead6b64e65fa64b9b3b3d3fce0
OLhaMels <olam67878@gmail.com>
2026-02-22 16:57:11 +0200

    creating conflict 2

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ function App() {
                             className={`filter-btn ${filter === 'all' ? 'active' : ''}`}
                             onClick={() => setFilter('all')}
                         >
-                            Усі ({total})
+                            Усі завдання ({total})
                         </button>
                         <button
                             className={`filter-btn ${filter === 'active' ? 'active' : ''}`}
